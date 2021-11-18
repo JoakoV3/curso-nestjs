eeb8add69cf58464cf1583d3f7a2d85910254652
cristianiniguez <felizpc1@gmail.com>
2021-11-17 21:27:24 -0400

    rest api: get - recibir parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,32 @@ export class AppController {
   @Get('/ruta/')
   otherEndpoint() {
     return 'hello again';
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=${limit} offset=${offset} brand=${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `product filter`;
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `product with id ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategories(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ) {
+    return `product with id ${productId} fromcategory with id ${id}`;
   }
 }
